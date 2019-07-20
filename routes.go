893808d3a930342d834fe1b351070733d840c9a9
Sagiruddin Mondal <sagir.sagir@@gmail.com>
2019-07-21 00:10:02 +0530

    restructure for routes and logger and models

--- routes.go
+++ routes.go
@@ -1,27 +1,58 @@
 package main
-
+ 
 import (
-	"net/http"
-
+    "net/http"
 	"github.com/gorilla/mux"
 )
-
+ 
+// Route is for routing 
 type Route struct {
-	Name        string
-	Method      string
-	Pattern     string
-	HandlerFunc http.HandleFunc
+    Name        string
+    Method      string
+    Pattern     string
+    HandlerFunc http.HandlerFunc
 }
-
+ 
+// Routes is for routing
 type Routes []Route
 
+// NewRouter is for route 
 func NewRouter() *mux.Router {
+ 
+    router := mux.NewRouter().StrictSlash(true)
+    for _, route := range routes {
+		var handler http.Handler
+ 
+        handler = route.HandlerFunc
+        handler = Logger(handler, route.Name)
 
+        router.
+            Methods(route.Method).
+            Path(route.Pattern).
+            Name(route.Name).
+            Handler(handler)
+    }
+ 
+    return router
+}
 
-	router := mux.Router {
-		router := mux.NewRou
-	}
-
-
-
+var routes = Routes{
+    Route{
+        "Index",
+        "GET",
+        "/",
+        Index,
+    },
+    Route{
+        "TodoIndex",
+        "GET",
+        "/todos",
+        TodoIndex,
+    },
+    Route{
+        "TodoShow",
+        "GET",
+        "/todos/{todoId}",
+        TodoShow,
+    },
 }
